033e389c0787f68959779ddd07afb97bc9f2fe69
finteger <104397474+finteger@users.noreply.github.com>
2024-03-25 13:23:28 -0600

    Update example.dart

--- example.dart
+++ example.dart
@@ -1,64 +1,145 @@
 class PhoneNumber {
-  String clean(String number) {
-    // regular expression gets rid of allowed punctuation
-    String num = number.replaceAll(RegExp(r'([()\s-.+])'), '');
 
-    // does the phone number have letters or unwanted punctuation?
-    if (num.contains(RegExp(r'[a-z]')))
-      throw FormatException('letters not permitted');
-    else if (num.contains(RegExp(r'[^\w]+')))
-      throw FormatException('punctuations not permitted');
+ String clean(String number) {
 
-    // if there are less than 10 digits or more than 11
-    if (num.length < 10)
-      throw FormatException('must not be fewer than 10 digits');
-    else if (num.length > 11)
-      throw FormatException('must not be greater than 11 digits');
+   // regular expression gets rid of allowed punctuation
 
-    // 11-digit numbers, the first digit must be a 1
-    if (num.length == 11) {
-      if (num[0] == '1') {
-        num = num.replaceFirst('1', '');
-      } else {
-        throw FormatException('11 digits must start with 1');
-      }
-    }
+   String num = number.replaceAll(RegExp(r'([()\s-.+])'), '');
 
-    // at this point, the number is 10 digits
+ 
 
-    // check if area code starts with 0 or 1
-    if (num[0] == '0')
-      throw FormatException('area code cannot start with zero');
-    else if (num[0] == '1')
-      throw FormatException('area code cannot start with one');
+   // does the phone number have letters or unwanted punctuation?
 
-    // check if exchange code starts with 0 or 1
-    if (num[3] == '0')
-      throw FormatException('exchange code cannot start with zero');
-    else if (num[3] == '1')
-      throw FormatException('exchange code cannot start with one');
+   if (num.contains(RegExp(r'[a-z]')))
 
-    return (num);
-  }
+     throw FormatException('letters not permitted');
+
+   else if (num.contains(RegExp(r'[^\w]+')))
+
+     throw FormatException('punctuations not permitted');
+
+ 
+
+   // if there are less than 10 digits or more than 11
+
+   if (num.length < 10)
+
+     throw FormatException('must not be fewer than 10 digits');
+
+   else if (num.length > 11)
+
+     throw FormatException('must not be greater than 11 digits');
+
+ 
+
+   // 11-digit numbers, the first digit must be a 1
+
+   if (num.length == 11) {
+
+     if (num[0] == '1') {
+
+       num = num.replaceFirst('1', '');
+
+     } else {
+
+       throw FormatException('11 digits must start with 1');
+
+     }
+
+   }
+
+ 
+
+   // at this point, the number is 10 digits
+
+ 
+
+   // check if area code starts with 0 or 1
+
+   if (num[0] == '0')
+
+     throw FormatException('area code cannot start with zero');
+
+   else if (num[0] == '1')
+
+     throw FormatException('area code cannot start with one');
+
+ 
+
+   // check if exchange code starts with 0 or 1
+
+   if (num[3] == '0')
+
+     throw FormatException('exchange code cannot start with zero');
+
+   else if (num[3] == '1')
+
+     throw FormatException('exchange code cannot start with one');
+
+ 
+
+   return (num);
+
+ }
+
+ 
+
+  String printNames([String retNames = '']){
+
+   return (retNames);
+
+ }
+
 }
 
+ 
+
+ 
 
 void main(){
-  
-  List<String> phoneNumbers = [
-'+1 (613)-995-0253',
-'613-995-0253',
-'1 613 995 0253',
-'613.995.0253'
-  ];
-  
-  
- var p = new PhoneNumber();
-  
-  for(var i = 0; i < phoneNumbers.length; i++){
-  
-   print(p.clean(phoneNumbers[i]));
-    
-  }
+
  
+
+  List<String> phoneNumbers = [
+
+'+1 (613)-995-0253',
+
+'613-995-0253',
+
+'1 613 995 0253',
+
+'613.995.0253'
+
+ ];
+
+ 
+
+  List<String> names = ['Fred','Sandy','Rowan','Peter'];
+
+ 
+
+  phoneNumbers.add('6139950253');
+
+ names.add('Janet');
+
+ names.sort();
+
+ 
+
+ var p = new PhoneNumber();
+
+ 
+
+  for(var i = 0; i < phoneNumbers.length; i++){
+
+ 
+
+   print(p.printNames(names[i]) + ' ' + p.clean(phoneNumbers[i]));
+
+   
+
+  }
+
+ 
+
 }
